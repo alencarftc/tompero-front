e86103694395259fab451025684d37f3ea993028
Felipe Alencar <alencar.itbam@outlook.com>
2020-04-24 21:15:07 -0400

    configured carousel and updated project to angular 9

--- src/app/layout/default-layout/default-layout.component.ts
+++ src/app/layout/default-layout/default-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tp-default-layout',
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./default-layout.component.scss']
 })
 export class DefaultLayoutComponent {
+  @Input() padding: boolean
 }
