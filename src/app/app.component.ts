8d47447eda52239a51d656ffe68d3ece23b99964
Felipe Alencar <alencar.itbam@outlook.com>
2020-05-20 14:15:50 -0400

    added dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  slideActivate(ngbSlideEvent: NgbSlideEvent) {
-    console.log(ngbSlideEvent.source);
-    console.log(ngbSlideEvent.paused);
-    console.log(NgbSlideEventSource.INDICATOR);
-    console.log(NgbSlideEventSource.ARROW_LEFT);
-    console.log(NgbSlideEventSource.ARROW_RIGHT);
-  }
-}
+export class AppComponent {}
